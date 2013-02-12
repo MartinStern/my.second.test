fc380a24fe62d660b7aacb4ed14c277ec210a4c5
MartinStern <martin.stern.dao@gmail.com>
2013-02-12 18:42:35 +0100

    first commit with IDEA from IntelliJ

---
+++ src/ch/daoinformati/martinsjavatestsnumbertwo/part1/SayNothingToTheWorld.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.daoinformati.martinsjavatestsnumbertwo.part1;
+
+/**
+ * @author MartinStern
+ */
+public class SayNothingToTheWorld {
+    public static void main(String attr[]) {
+        System.err.println("ha ha ");
+        System.out.println("und das hier kommt aus IDEA von IntelliJ");
+        // echt schräg so was
+    }
+
+}
