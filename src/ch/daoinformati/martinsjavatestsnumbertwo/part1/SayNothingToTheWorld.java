2a1d59230426bf0f379cf16e0c63c94caf259ecf
MartinStern <martin.stern.dao@gmail.com>
2013-02-12 18:42:35 +0100

    an der struktur gefummelt

---
+++ src/ch/daoinformati/martinsjavatestsnumbertwo/part1/SayNothingToTheWorld.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.daoinformati.martinsjavatestsnumbertwo.part1;
+
+/**
+ *
+ * @author MartinStern
+ */
+public class SayNothingToTheWorld {
+    public static void main(String attr[]){
+        System.err.println("ha ha ");
+        // echt schräg so was
+    }
+    
+}
