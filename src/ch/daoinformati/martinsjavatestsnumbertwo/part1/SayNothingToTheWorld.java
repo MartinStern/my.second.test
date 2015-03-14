568aa7f2a686e18cddd03c5120edcac985488155
MartinStern <martin.stern.dao@gmail.com>
2015-03-14 17:11:20 +0100

    first commit with IDEA from IntelliJ

--- src/ch/daoinformati/martinsjavatestsnumbertwo/part1/SayNothingToTheWorld.java
+++ src/ch/daoinformati/martinsjavatestsnumbertwo/part1/SayNothingToTheWorld.java
@@ -8,10 +8,29 @@ package ch.daoinformati.martinsjavatestsnumbertwo.part1;
  * @author MartinStern
  */
 public class SayNothingToTheWorld {
+    public String getTestString() {
+        return testString;
+    }
+
+    public void setTestString(String testString) {
+        this.testString = testString;
+    }
+
+    String testString;
     public static void main(String attr[]) {
         System.err.println("ha ha ");
         System.out.println("und das hier kommt aus IDEA von IntelliJ");
         // echt schräg so was
+        SayNothingToTheWorld me = new SayNothingToTheWorld();
+        me.setTestString("bla bla bla ....");
+        System.out.println("und was steht nun drin: " + me.getTestString());
+        System.out.println(me.toString());
     }
 
+    @Override
+    public String toString() {
+        return "SayNothingToTheWorld{" +
+                "testString='" + testString + '\'' +
+                '}';
+    }
 }
